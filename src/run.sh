cebae5b3934a645b49b73f0eaa4d455915042c0e
Ankith Boggaram <ankith.boggaram2001@gmail.com>
2024-10-20 09:58:13 -0600

    Edit run.sh to include 5 clients

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,7 @@
 python run_server.py &
 python run_client.py --config ./client_1.yaml &
 python run_client.py --config ./client_2.yaml &
+python run_client.py --config ./client_3.yaml &
+python run_client.py --config ./client_4.yaml &
+python run_client.py --config ./client_5.yaml
 wait
