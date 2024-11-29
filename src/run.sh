a03c1fcb2f47cba83aa0413ed912a24a36daed2e
Ankith Boggaram <ankith.boggaram2001@gmail.com>
2024-11-29 13:13:34 -0700

    Update run.sh and server config

--- src/run.sh
+++ src/run.sh
@@ -4,6 +4,5 @@ python run_server.py &
 python run_client.py --config ./client_1.yaml &
 python run_client.py --config ./client_2.yaml &
 python run_client.py --config ./client_3.yaml &
-python run_client.py --config ./client_4.yaml &
-python run_client.py --config ./client_5.yaml
+python run_client.py --config ./client_4.yaml
 wait
